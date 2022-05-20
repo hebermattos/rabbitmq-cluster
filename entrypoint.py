97f32e8494810516eff1081ff492adb9a628fb64
Heber Mattos <heber.mattos@ilegra.com>
2022-05-20 16:26:37 -0300

    removendo vhost

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-import os
-import sys
 import time
 import logging
-import re
 import socket
 from ast import literal_eval
 from subprocess import check_call, check_output, Popen, PIPE
@@ -31,60 +28,39 @@ def check_tcp_port(host, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((host, port))
-
+        
         return result == 0
     except:
         return False
 
-
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    vhost = os.getenv('RABBITMQ_DEFAULT_VHOST')
-
-
-    if vhost is None:
-        logging.info("Non cluster mode")
-        os.execv('/bin/bash', ['/bin/bash', 'docker-entrypoint.sh', 'rabbitmq-server'])
-    else:
-        logging.info("Cluster mode")
-        process = Popen(['docker-entrypoint.sh', 'rabbitmq-server'])
-
-        while not check_tcp_port('127.0.0.1', 5672):
-            logging.info('Waiting local node...')
-            time.sleep(1)
-
-        while not healthcheck():
-            logging.info("Waiting for...")
-            time.sleep(1)
-
-        node_name = cluster_status()
-
-        if node_name in vhost:
-            process.wait()
-            return exit(process.returncode)
-
-        while not check_tcp_port(vhost, 4369):
-            logging.info('Waiting node...')
-            time.sleep(1)
-
-        count = 5
-        for i in range(count):
-            try:
-                check_call(['rabbitmqctl', 'stop_app'])
-
-                logging.info("Joining cluster")
-                check_call(['rabbitmqctl', 'join_cluster', 'rabbit@%s' % vhost])
-                check_call(['rabbitmqctl', 'start_app'])
-                break
-            except:
-                logging.info('Retrying to joining cluster')
-                time.sleep(5)
-                continue
-
-        process.wait()
-        return exit(process.returncode)
-
+    logging.info("Cluster mode")
+    process = Popen(['docker-entrypoint.sh', 'rabbitmq-server'])
+    while not check_tcp_port('127.0.0.1', 5672):
+        logging.info('Waiting local node...')
+        time.sleep(1)
+    while not healthcheck():
+        logging.info("Waiting for...")
+        time.sleep(1)
+    while not check_tcp_port('rabbitmq1', 4369):
+        logging.info('Waiting node...')
+        time.sleep(1)
+    count = 5
+    for i in range(count):
+        try:
+            check_call(['rabbitmqctl', 'stop_app'])
+            logging.info("Joining cluster")
+            check_call(['rabbitmqctl', 'join_cluster', 'rabbit@rabbitmq1'])
+            check_call(['rabbitmqctl', 'start_app'])
+            break
+        except:
+            logging.info('Retrying to joining cluster')
+            time.sleep(5)
+            continue
+    process.wait()
+    return exit(process.returncode)
 
 if __name__ == '__main__':
     main()
